e275a7baaf572dc2d5b98827f6234e1fb445fccb
Matthew Whigham <matthewwhigham@mindsetconsulting.com>
2020-03-24 20:51:03 +0000

    contact updated

--- webapp/ext/Contact/Component.js
+++ webapp/ext/Contact/Component.js
@@ -1,19 +1,21 @@
-(function () {
-	"use strict";
+/* global sap */
+sap.ui.define(["sap/ovp/cards/generic/Component"],
+ 
+    function (CardComponent) {
+        "use strict";
+ 
+        return CardComponent.extend("com.mindset.appanalyzer.ext.Contact.Component", {
 
-	/* component for custom card */
-
-	jQuery.sap.declare("com.mindset.appanalyzer.ext.Contact.Component");
-	jQuery.sap.require("sap.ovp.cards.custom.Component");
-
-	sap.ovp.cards.custom.Component.extend("com.mindset.appanalyzer.ext.Contact.Component", {
-		// use inline declaration instead of component.json to save 1 round trip
 		metadata: {
 			properties: {
 				"contentFragment": {
 					"type": "string",
 					"defaultValue": "com.mindset.appanalyzer.ext.Contact.Contact"
 				},
+				"controllerName": {
+                        "type": "string",
+                        "defaultValue": "com.mindset.appanalyzer.ext.Contact.Contact"
+                },
 				"headerFragment": {
 					"type": "string",
 					"defaultValue": ""
@@ -34,14 +36,7 @@
 				libs: ["sap.m"],
 				components: []
 			},
-			config: {},
-			customizing: {
-				"sap.ui.controllerExtensions": {
-					"sap.ovp.cards.generic.Card": {
-						controllerName: "com.mindset.appanalyzer.ext.Contact.Contact"
-					}
-				}
-			}
+			config: {}
 		}
 	});
-})();
+});
