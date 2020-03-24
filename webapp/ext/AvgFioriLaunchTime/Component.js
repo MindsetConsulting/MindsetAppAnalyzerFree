1f32747938d9127af1abbd11b75496791a41a284
mw9 <36144660+msw009@users.noreply.github.com>
2020-03-24 13:14:52 -0500

    close issue #6: added network calls for load time (#42)
    * Issue6 initial: added front end model
    
    * added fragment definition
    
    * added READ
    
    * added Avg Network time
    
    * ESLint fixes
    
    * fixed component.js
    
    * test for ESLint
    
    * fixed ESLint
    
    * removed jQuery
    
    * adj to ()
    
    * fixed the sap.ui.define issues
    
    Co-authored-by: Matthew Whigham <matthewwhigham@mindsetconsulting.com>

--- webapp/ext/AvgFioriLaunchTime/Component.js
+++ webapp/ext/AvgFioriLaunchTime/Component.js
@@ -1,19 +1,21 @@
-(function () {
-	"use strict";
+/* global sap */
+sap.ui.define(["sap/ovp/cards/generic/Component"],
+ 
+    function (CardComponent) {
+        "use strict";
+ 
+        return CardComponent.extend("com.mindset.appanalyzer.ext.AvgFioriLaunchTime.Component", {
 
-	/* component for custom card */
-
-	jQuery.sap.declare("com.mindset.appanalyzer.ext.AvgFioriLaunchTime.Component");
-	jQuery.sap.require("sap.ovp.cards.custom.Component");
-
-	sap.ovp.cards.custom.Component.extend("com.mindset.appanalyzer.ext.AvgFioriLaunchTime.Component", {
-		// use inline declaration instead of component.json to save 1 round trip
 		metadata: {
 			properties: {
 				"contentFragment": {
 					"type": "string",
 					"defaultValue": "com.mindset.appanalyzer.ext.AvgFioriLaunchTime.AvgFioriLaunchTimes"
 				},
+				"controllerName": {
+                        "type": "string",
+                        "defaultValue": "com.mindset.appanalyzer.ext.AvgFioriLaunchTime.AvgFioriLaunchTimes"
+                },
 				"headerFragment": {
 					"type": "string",
 					"defaultValue": ""
@@ -34,14 +36,14 @@
 				libs: ["sap.m"],
 				components: []
 			},
-			config: {},
-			customizing: {
+			config: {}
+			/*customizing: {
 				"sap.ui.controllerExtensions": {
 					"sap.ovp.cards.generic.Card": {
 						controllerName: "com.mindset.appanalyzer.ext.AvgFioriLaunchTime.AvgFioriLaunchTimes"
 					}
 				}
-			}
+			}*/
 		}
 	});
-})();
+});
