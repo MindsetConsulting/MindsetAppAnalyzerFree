656ac92c81215cb767aade94443b9f00caa5210f
Matthew Whigham <matthewwhigham@mindsetconsulting.com>
2020-04-21 18:25:06 +0000

    initial VoE

--- webapp/ext/Feedback/Component.js
+++ webapp/ext/Feedback/Component.js
@@ -1,47 +1,49 @@
-sap.ui.define(["sap/ovp/cards/custom/Component"], function (Component) {
-	"use strict";
+/* global sap */
+sap.ui.define(["sap/ovp/cards/custom/Component"],
 
-	/* component for custom card */
-	return Component.extend("com.mindset.appanalyzer.ext.Feedback.Component", {
-		// use inline declaration instead of component.json to save 1 round trip
-		metadata: {
-			properties: {
-				"contentFragment": {
-					"type": "string",
-					"defaultValue": "com.mindset.appanalyzer.ext.Feedback.Feedback"
+	function (Component) {
+		"use strict";
+
+		return Component.extend("com.mindset.appanalyzer.ext.Feedback.Component", {
+			
+			metadata: {
+				properties: {
+					"contentFragment": {
+						"type": "string",
+						"defaultValue": "com.mindset.appanalyzer.ext.Feedback.Feedback"
+					},
+					"controllerName": {
+						"type": "string",
+						"defaultValue": "com.mindset.appanalyzer.ext.Feedback.Feedback"
+					},
+					"headerFragment": {
+						"type": "string",
+						"defaultValue": ""
+					},
+					"footerFragment": {
+						"type": "string",
+						"defaultValue": ""
+					}
 				},
-				"controllerName": {
-                        "type": "string",
-                        "defaultValue": "com.mindset.appanalyzer.ext.Feedback.Feedback"
-                },
-				"headerFragment": {
-					"type": "string",
-					"defaultValue": ""
+
+				version: "@version@",
+
+				library: "sap.ovp",
+
+				includes: [],
+
+				dependencies: {
+					libs: ["sap.m"],
+					components: []
 				},
-				"footerFragment": {
-					"type": "string",
-					"defaultValue": ""
-				}
-			},
-
-			version: "@version@",
-
-			library: "sap.ovp",
-
-			includes: [],
-
-			dependencies: {
-				libs: ["sap.m"],
-				components: []
-			},
-			config: {},
-			customizing: {
-				"sap.ui.controllerExtensions": {
-					"sap.ovp.cards.generic.Card": {
-						controllerName: "com.mindset.appanalyzer.ext.Feedback.Feedback"
+				config: {},
+				customizing: {
+					"sap.ui.controllerExtensions": {
+						"sap.ovp.cards.generic.Card": {
+							controllerName: "com.mindset.appanalyzer.ext.Feedback.Feedback"
+						}
 					}
 				}
 			}
-		}
+		});
 	});
-});
--- webapp/ext/Feedback/Feedback.controller.js
+++ webapp/ext/Feedback/Feedback.controller.js
@@ -1,33 +1,49 @@
 /* global sap */
-sap.ui.define(["sap/ovp/cards/generic/Card.controller"
-	],
+sap.ui.define(["sap/ovp/cards/generic/Card.controller"],
 	function (Controller) {
 		"use strict";
 		return Controller.extend("com.mindset.appanalyzer.ext.Feedback.Feedback", {
-		
+
 			onInit: function () {
-			var oView = this.getView();
-			var that = this;
-			var oUserFeedbackModelData = {
-				"AVGRating": ""
-			};
-			var oUserFeedbackModel = new sap.ui.model.json.JSONModel(oUserFeedbackModelData);
-			oView.setModel(oUserFeedbackModel, "oUserFeedbackModel");
-			var sUrl = "/sap/opu/odata/MINDSET/FIORI_MONITOR_SRV/";
-			var oDataModel = new sap.ui.model.odata.ODataModel(sUrl, false);
-			oView.setModel(oDataModel);
-			var sPath = "/FeedbackSet/$count";
-			oDataModel.read(sPath, {
-				success: function (oData, oRes) {
-					oUserFeedbackModel.setProperty("/AVGRating", oRes.body);
-					oUserFeedbackModel.updateBindings(true);
-				},
-				error: function (data) {
+				var oView = this.getView();
+				var that = this;
+				var oUserFeedbackModelData = {
+					"AVGRating": ""
+				};
+				var oUserFeedbackModel = new sap.ui.model.json.JSONModel(oUserFeedbackModelData);
+				oView.setModel(oUserFeedbackModel, "oUserFeedbackModel");
+				var sUrl = "/sap/opu/odata/MINDSET/FIORI_MONITOR_SRV/";
+				var oDataModel = new sap.ui.model.odata.ODataModel(sUrl, false);
+				oView.setModel(oDataModel);
+				var sPath = "/FeedbackSet/$count";
+				oDataModel.read(sPath, {
+					success: function (oData, oRes) {
+						var avgnum = that.getView().getId("idAVG");
+						switch (oRes.body) {
+						case 1:
+							avgnum.addStyleClass("feedbackRed");
+							break;
+						case 2:
+							avgnum.addStyleClass("feedbackRed");
+							break;
+						case 3:
+							avgnum.addStyleClass("feedbackYellow");
+							break;
+						case 4:
+							avgnum.addStyleClass("feedbackGreen");
+							break;
+						case 5:
+							avgnum.addStyleClass("feedbackGreen");
+							break;
+						}
+						oUserFeedbackModel.setProperty("/AVGRating", oRes.body);
+						oUserFeedbackModel.updateBindings(true);
+					},
+					error: function (data) {
 
-				}
-			});
+					}
+				});
 
-				
 			},
 
 			onAfterRendering: function () {
