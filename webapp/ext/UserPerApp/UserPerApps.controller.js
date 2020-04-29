31a2763352801e335cb230dc8289ab11dad00cb0
Matthew Whigham <matthewwhigham@mindsetconsulting.com>
2020-04-29 21:42:31 +0000

    added a details move in userperapp

--- webapp/ext/UserPerApp/UserPerApps.controller.js
+++ webapp/ext/UserPerApp/UserPerApps.controller.js
@@ -7,37 +7,6 @@
 
 		onInit: function () {
 			var me = this;
-			//Call the method when the route and pattern matches
-
-			/*	
-				
-					"card07": {
-				"model": "fiorimoni",
-				"template": "com.mindset.appanalyzer.ext.UserPerApp",
-				"settings": {
-					"title": "{{UserPerApp}}",
-					"defaultSpan": {
-						"cols": 3,
-						"rows": 8
-					}
-				}
-			}
-			
-			 */
-
-			/* 
-			 	"card07": {
-				"model": "fiorimoni",
-				"template": "sap.ovp.cards.charts.analytical",
-				"settings": {
-					"title": "{{AvgTimeOnApps}}",
-					"entitySet": "AppLogInSet",
-					"chartAnnotationPath": "com.sap.vocabularies.UI.v1.Chart",
-					"navigation": "dataPointNav"
-				}
-			}
-			 
-			 */
 			var oView = me.getView();
 			var oUserPerAppModel = new sap.ui.model.json.JSONModel();
 			me.getView().setModel(oUserPerAppModel, "oUserPerAppModel");
@@ -68,8 +37,9 @@
 					text: 'Appviews Today by App'
 				}
 			});
+
 			oVizFrame.setModel(oUserPerAppModel);
-      	var sUrl = "/sap/opu/odata/MINDSET/FIORI_MONITOR_SRV/";
+			var sUrl = "/sap/opu/odata/MINDSET/FIORI_MONITOR_SRV/";
 			// var sUrl = "/sap/opu/odata/sap/ZMND_FIORI_MONITOR_SRV/";
 			var oDataModel = new sap.ui.model.odata.ODataModel(sUrl, true, "", "");
 			oView.setModel(oDataModel);
@@ -85,17 +55,28 @@
 				}
 			});
 			this.byId("idVizFrame").setLegendVisible(false);
+
+			var oPopOver = this.getView().byId("idPopOver");
+			oPopOver.connect(oVizFrame.getVizUid());
+			oPopOver.setActionItems({
+				type: 'action',
+				text: 'Details',
+				press: function () {
+					console.log('This is a callback function from "Action Button" Action.');
+				}
+			});
 		},
-        dataSort: function(dataset) {
-            //let data sorted by revenue
-            if (dataset && dataset.hasOwnProperty("results")) {
-                var arr = dataset.results;
-                arr = arr.sort(function (a, b) {
-                    return b.Pageviews - a.Pageviews;
-                });
-                return dataset;
-            }
-        },
+
+		dataSort: function (dataset) {
+			//let data sorted by revenue
+			if (dataset && dataset.hasOwnProperty("results")) {
+				var arr = dataset.results;
+				arr = arr.sort(function (a, b) {
+					return b.Pageviews - a.Pageviews;
+				});
+				return dataset;
+			}
+		},
 		onAfterRendering: function () {
 
 		},
